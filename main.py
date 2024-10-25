a9e0c07b4aef061a8096bc570595d8ffcaeb8b31
mohammed faizan <faizan1999.in@gmail.com>
2024-10-25 15:47:03 +0530

    created player class, asteroid, circleshape for objects

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting asteroids!")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Screen width:", SCREEN_WIDTH)
-    print("Screen height:", SCREEN_HEIGHT)
-    WHITE = (255, 255, 255)
-    BLACK = (0, 0, 0)
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(WHITE)
+
+        for obj in updatable:
+            obj.update(dt)
+
+
+        
+        screen.fill("black")
+    
+        for obj in drawable:
+            obj.draw(screen)
+            
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
